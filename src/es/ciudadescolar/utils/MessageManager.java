8d34f897bcdf2c213e5e3b5e020097222ca74da1
cgaq100 <cgaq100@gmail.com>
2025-09-21 13:46:00 +0200

    Poner nombre de usuario en mayuscula

--- src/es/ciudadescolar/utils/MessageManager.java
+++ src/es/ciudadescolar/utils/MessageManager.java
@@ -3,6 +3,6 @@ package es.ciudadescolar.utils;
 public class MessageManager {
     public static void EscribirMensaje(){
         System.out.println("Empezamos a usar Repositorios de Código...");
-        System.out.println("Usuario => " + System.getProperty("user.name"));
+        System.out.println("Usuario => " + System.getProperty("user.name").toUpperCase());
     }
 }
