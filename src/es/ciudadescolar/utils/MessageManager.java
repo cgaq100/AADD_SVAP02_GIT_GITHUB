4653ffe69bc4734b04b932a374d59f964a78d6bb
cgaq100 <cgaq100@gmail.com>
2025-09-21 13:44:55 +0200

    Añadir mensaje con el nombre de usuario

--- src/es/ciudadescolar/utils/MessageManager.java
+++ src/es/ciudadescolar/utils/MessageManager.java
@@ -3,5 +3,6 @@ package es.ciudadescolar.utils;
 public class MessageManager {
     public static void EscribirMensaje(){
         System.out.println("Empezamos a usar Repositorios de Código...");
+        System.out.println("Usuario => " + System.getProperty("user.name"));
     }
 }
